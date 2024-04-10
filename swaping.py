1e117ac78a848da77ae2729ddea9b537429bf099
kaziizharul <kaziizharul123@gmail.com>
2024-04-10 14:05:19 +0600

    first python commit

---
+++ swaping.py
@@ -0,0 +1,13 @@
+#swaping
+x=input('Enter the value of x=')
+y=input('Enter the value of y=')
+
+swap=x
+x=y
+y=swap
+
+print('the swaping value of x is:',x)
+print('the swaping value of y is:',y)
+
+
+
